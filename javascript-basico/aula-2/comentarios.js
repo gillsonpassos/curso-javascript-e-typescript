33637e5a9e7a43be7e0adda740e3b2e570be6b1e
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-15 15:19:55 -0300

    comentarios de codigos

---
+++ javascript-basico/aula-2/comentarios.js
@@ -0,0 +1,10 @@
+// escrever um comentsrio
+console.log("olá mundo"); // aqui tem outro comentario
+console.log("linha 1");
+console.log("linha 2");
+console.log("linha 3");
+console.log("linha 4");
+// vai ser ignorado comenta uma linha
+/*
+comenta um bloco
+*/
