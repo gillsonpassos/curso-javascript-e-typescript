a8c0b873787070cffa21c732b6797a1aecad47f3
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-16 15:51:55 -0300

    constantes com-const

---
+++ javascript-basico/aula-5/index.js
@@ -0,0 +1,9 @@
+// string = text | number = numero
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultaDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
+
+console.log(primeiroNumero + segundoNumero);
