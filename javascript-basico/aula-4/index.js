b7394a3e322defb5ab4c6e69c5d65307f4688c37
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-16 12:35:11 -0300

    Variaveis com let

---
+++ javascript-basico/aula-4/index.js
@@ -0,0 +1,12 @@
+// não podemos criar variaveis com palavras reservadas.
+// variaveis precisam ter nomes significativos.
+// Não pode começar o nome de uma variavel com numero.
+// variveis não podem conter espaços ou traços.
+// utilizamos camelcase.
+// Case-Sensitive.
+// Não podemos redeclarar variaveis com let.
+// NÃO UTILIZE VAR, UTILIZE LET .
+let nomeCliente = "LUIZ";
+nomeCliente = "OTÁVIO";
+
+console.log("nomeCliente");
---
+++ javascript-basico/aula-4/testes.js
@@ -0,0 +1,7 @@
+let nome = "Matusalem"; //string
+
+console.log(nome, "nasceu em 1984.");
+console.log("Em 2000", nome, "conheceu maria.");
+console.log(nome, "casou-se com maria em 2012.");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O Filho de", nome, "se chama Eduardo.");
