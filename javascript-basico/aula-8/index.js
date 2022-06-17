a2f94007d3a859a23dd36bc9ecb4dda07e365e80
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-17 10:19:35 -0300

    Tipos de dados Primitivos

---
+++ javascript-basico/aula-8/index.js
@@ -0,0 +1,15 @@
+const ford = "Bronco"; // string
+const ford1 = "Bronco"; // string
+const ford2 = `Bronco`; // string
+const num = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // underfined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória
+const aprovado = true; // boolean = true, false. (valor lógico).
+
+let a = 2;
+const b = a;
+console.log(a, b); // 2, 2
+
+a = 3;
+console.log(a, b); // 3, 2
