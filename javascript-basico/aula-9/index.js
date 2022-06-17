333f330b2cf87f631fe00d6efa5a9581dc9304db
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-17 13:01:56 -0300

    Operadores-Aritméticos, de Atribuição e Incremento

---
+++ javascript-basico/aula-9/index.js
@@ -0,0 +1,17 @@
+/*
+() primeiro
+** segundo
+*  /  % terceiro
++ -
+*/
+
+/*const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+NaN - not a number  parseInt (inteiro), parsefloat(decimais)
+console.log((num1 + num2) * num3);*/
+
+const num1 = 10;
+const num2 = parseFloat("5.2");
+console.log(num1 + num2);
+console.log(typeof num2);
---
+++ javascript-basico/aula-9/tests.js
@@ -0,0 +1,24 @@
+/**
+ *operadores aritméticos
+ * +  Adição / Concatenação
+ * - / * subtração, divisão, multiplicação
+ *  ** Potenciação (ELEVADO)
+ * % resto da divisão
+ */
+
+/*
+() primeiro
+** segundo
+*  /  % terceiro
++ -
+*/
+/*
+Incremento = ++
+Decremento = --
+*/
+/*let contador = 0;
+contador += 2;
+contador += 2;
+contador += 2;
+contador += 2;
+console.log(contador);*/
