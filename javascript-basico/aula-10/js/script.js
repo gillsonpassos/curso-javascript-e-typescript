462792298ec61cd2b265c34811e8ce35c0173dcf
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-17 17:23:14 -0300

    alert-confirm-prompt-(Exercicios)

---
+++ javascript-basico/aula-10/js/script.js
@@ -0,0 +1,7 @@
+/*alert("Nossa mensagem");*/
+let num1 = prompt("Digite um numero");
+let num2 = prompt("Digite outro numero");
+num1 = Number(num1);
+num2 = parseFloat(num2);
+const resultado = num1 + num2;
+alert(`O resultado foi  ${resultado}`);
