ba284c27d0c2e4231a18652ba6e715fb7f30d7e7
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-20 11:38:21 -0300

    Mais sobre Strings

---
+++ javascript-basico/aula-12/index.js
@@ -0,0 +1,14 @@
+//               01234567
+//let umaString = "um texto";
+
+/*console.log(umaString.concat(" em um lindo dia."));
+console.log(umaString + " em um lindo dia.");
+console.log(`${umaString} em um lindo dia.`);*/
+
+//console.log(umaString.indexOf("texto"));
+//                0123456789
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+//console.log(umaString. length - 3);
+console.log(umaString.slice(-5));
+//console.log(umaString.slice(32));
