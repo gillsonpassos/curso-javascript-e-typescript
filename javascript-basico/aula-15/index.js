96eb1d6bea2e11c930c255b0d5345f406c9d9241
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-21 08:02:08 -0300

    Objeto Math

---
+++ javascript-basico/aula-15/index.js
@@ -0,0 +1,14 @@
+console.log(100 / 0);
+
+// let num1 = 16;
+// console.log(num1 ** 0.5);
+// let num2 = Math.floor(num1); // arredonda o numero pra baixo.
+// let num2 = Math.ceil(num1); // arredonda o numero pra cima.
+// let num2 = Math.round(num1);
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(aleatorio);
+// console.log(Math.PI);
+// console.log(Math.pow(2, 10));
+// console.log(2 ** 10);
