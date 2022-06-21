335da8de14e60806d4e85bc350e407a2b7a32cac
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-21 14:24:19 -0300

    Arrays (Basico)

---
+++ javascript-basico/aula-17/index.js
@@ -0,0 +1,40 @@
+//            012345678910
+// const nome = "Gilson Passos";
+// console.log(nome[10]);
+//               0         1       2
+let alunos = ["Gilson", "Arly", "Flavio"]; // Gilson, Flavio
+alunos = 12; // Gilson, Flavio
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push("Gilson");
+// alunos.push("Flavio");
+
+// console.log(alunos.slice(0, 5));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos);
+
+// const removido = alunos.shift();
+// console.log(removido);
+
+// console.log(alunos);
+
+// alunos.unshift("Alice");
+// alunos.unshift("Paulo"); // adiciona no começo.
+// alunos.unshift("Renata");
+
+// alunos.push("Otávio"); //adiciona no fim.
+// alunos.push("Luiza");
+// alunos[alunos.length] = "LUIZA"; // adiciona no fim.
+// alunos[alunos.length] = "Fabio";
+
+// alunos[0] = "Renata";
+// alunos[3] = "Gilson";
+
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
