d408d14ecc972c6cc1d630d7044a1cb2ec08607b
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-21 16:37:21 -0300

    Funções Basicas

---
+++ javascript-basico/aula-18/rascunho.js
@@ -0,0 +1,15 @@
+// function saudação(nome) {
+//   return `Bom dia ${nome}!`;
+// }
+
+// const variavel = saudação("Luiz");
+// console.log(variavel);
+
+function soma(x, y) {
+  const resultado = x + y;
+  return resultado;
+}
+
+console.log(soma(2, 2));
+console.log(soma(3, 1));
+console.log(soma(5, 10));
