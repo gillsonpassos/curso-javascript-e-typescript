9b1683933cc38d714b33045f4ed016f74a80c980
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-23 08:40:23 -0300

    Operadores de Comparação.

---
+++ javascript-basico/aula-22/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igualdade (valor)
+=== Igualdade estrita (valor e tipo)
+!= Diferente (valor)
+!== Diferente estrito (valor e tipo)
+*/
+const num1 = 10; // number
+const num2 = "10"; // string
+const comp = num1 === num2;
+console.log(comp);
