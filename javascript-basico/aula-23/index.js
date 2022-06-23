c915d5204c2faa071aba9e3b16559b70f72f9ccc
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-23 09:42:25 -0300

    Operadores Lógicos

---
+++ javascript-basico/aula-23/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -->Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressãoAND = true && true && true && true;
+// const expressãoOr = true || false || true || false;
+
+// const usuário = "LUIZ";
+// const senha = "123456";
+// //                       true                false
+// const vaiLogar = usuário === "LUIZ" && senha === "12345";
+
+console.log(true);
