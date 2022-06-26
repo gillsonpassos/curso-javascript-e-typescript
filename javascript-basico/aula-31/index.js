7e5665a33c7ba5dd2f96bf952a565de13c3b91c4
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-26 08:41:28 -0300

    Mais diferenças entre-var-let/const

---
+++ javascript-basico/aula-31/index.js
@@ -0,0 +1,32 @@
+const verdadeira = true;
+
+//  Let tem escopo de bloco {....Bloco}
+//  var só tem escopo de função
+
+// let nome = "Luiz"; // aqui esta criando uma variavel
+// var nome2 = "Luiz";
+
+// var nome2 = "Otavio"; // Redeclarada
+
+// if (verdadeira) {
+//   let nome = "Otavio"; // aqui esta criando outra variavel
+//   var nome2 = "Rogerio"; // Redeclarada
+
+//   if (verdadeira) {
+//     var nome2 = "Ronaldo"; // Redeclarada
+//     let nome = "Outra Coisa";
+//   }
+// }
+
+// console.log(nome, nome2);
+
+// var sobrenome = 'Miranda';
+
+// function falaOi () {
+//     console.log(sobrenome);
+// }
+// falaOi();
+
+console.log(sobrenome);
+
+var sobrenome = "Miranda";
