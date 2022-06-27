51c891ac83838c58d237153efc1cf56c9bb6f330
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-27 11:19:53 -0300

    For-Clássico-Estrutura de Repetição

---
+++ javascript-basico/aula-34/index.js
@@ -0,0 +1,14 @@
+const frutas = [
+  "Maçã",
+  "Pêra",
+  "Uva",
+  "morango",
+  "Gilson",
+  "Miranda",
+  "jorgina",
+  "Tay",
+];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`indice ${i}`, frutas[i]);
+}
