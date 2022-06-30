6a3654fae0de2de7c23ccde0f533841e373803f3
gillsonpassos <oliveirapassos07@outlook.com>
2022-06-30 09:48:59 -0300

    Exercicio com lógica de programação-3

---
+++ javascript-basico/aula-43/exercicio.js
@@ -0,0 +1,8 @@
+// Escreva uma função que receba um numero e
+// retorne o seguinte:
+// Numero é divisível por 3 = Fizz
+// Numero é divisível por 5 = Buzz
+// Numero é divisível por 3 e 5 = FizzBuzz
+// Numero Não é divisível por 3 e 5 = retorna o próprio número
+// Checar se o número é realmente um numero = retorna o próprio número
+// Use a função com numeros de 0 a 100
