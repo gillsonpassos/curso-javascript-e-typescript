37a2ae50916eb0ffc675ba3dcaadf7593555ef1e
gillsonpassos <oliveirapassos07@outlook.com>
2022-07-01 14:48:00 -0300

    Exercicio-Criando um timer com setInterval

---
+++ javascript-basico/aula-47/assets/js/main.js
@@ -0,0 +1,47 @@
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "UTC",
+    });
+  }
+
+  const relogio = document.querySelector(".relogio");
+  const iniciar = document.querySelector(".iniciar");
+  const pausar = document.querySelector(".pausar");
+  const zerar = document.querySelector(".zerar");
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener("click", function (e) {
+    console.log(e.target);
+  });
+
+  iniciar.addEventListener("click", function (event) {
+    relogio.classList.remove("pausado");
+    clearInterval(timer);
+    iniciaRelogio();
+  });
+
+  pausar.addEventListener("click", function (event) {
+    relogio.classList.add("pausado");
+    clearInterval(timer);
+  });
+
+  zerar.addEventListener("click", function (event) {
+    relogio.classList.remove("pausado");
+    clearInterval(timer);
+    clearInterval(timer);
+    relogio.innerHTML = "00:00:00";
+    segundos = 0;
+  });
+}
+relogio();
