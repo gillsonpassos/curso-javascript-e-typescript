2b76c939e4a7de6316909e60f759e2a7bdfab58b
gillsonpassos <oliveirapassos07@outlook.com>
2022-07-12 08:57:35 -0300

    Revisão do basico em Arrays

---
+++ javascript-basico/aula-62/index.js
@@ -0,0 +1,16 @@
+// push adiciona no final do Arrays
+// unshift adciona no começo do Arrays
+// pop remove do final e shift remove do começo
+// Valor por Referência
+//                 0         1        2
+//const nomes = ["Eduardo", "Maria", "Joana", "wallace", "Rosana"];
+// const novo = [...nomes];
+//const novo = nomes.slice(1, -1);
+// novo.pop();
+// console.log(nomes);
+//console.log(novo);
+
+//const nome = "Gilson Passos Oliveira";
+const nomes = ["Gilson", "Passos", "Oliveira"];
+const nome = nomes.join(" ");
+console.log(nome);
