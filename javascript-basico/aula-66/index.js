3f4af44c00a5bb122b6c0ebeef710cdbea548280
gillsonpassos <oliveirapassos07@outlook.com>
2022-07-13 11:49:04 -0300

    Map-Mapeando o Array

---
+++ javascript-basico/aula-66/index.js
@@ -0,0 +1,39 @@
+// Dobre os números
+//               0  1   2   3  4  5 ........
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const numerosEmDobro = numeros.map((valor) => valor * 2);
+// console.log(numerosEmDobro);
+
+//############//############//###########
+
+// Para cada elemento
+// Retorne apenas uma string com nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto (id)
+
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Leticia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
+const nomes = pessoas.map((obj) => obj.nome);
+const idades = pessoas.map((obj) => ({ idade: obj.idade }));
+
+const comIds = pessoas.map(function (obj, indice) {
+  const newObj = { ...obj };
+  newObj.id = indice;
+  return newObj;
+});
+
+//console.log(pessoas);
+console.log(comIds);
+
+//const pessoasComNomeGrande = pessoas.filter((obj) => obj.nome.length >= 7);
+//   const pessoasComMaisDeCinquentaAnos = pessoas.filter((obj) => obj.idade > 50);
+//   const nomeTerminaComA = pessoas.filter((obj) => {
+//     return obj.nome.toLowerCase().endsWith("a");
+//   });
+//   console.log(nomeTerminaComA);
