ea72e99cd39467d0dc0e8530289f37d9da2b4f95
gillsonpassos <oliveirapassos07@outlook.com>
2022-07-13 09:39:35 -0300

    Filter-Filtrando o Array

---
+++ javascript-basico/aula-65/index.js
@@ -0,0 +1,37 @@
+// Filter, map, reduce
+// Filter -> Vai sempre retornar um array,
+//com a mesma quantidade de elementos, ou menos.
+//
+// Retorne os numeros maiores que 10
+//               0  1   2   3  4  5 ........
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+// function callbackFilter(valor) {
+//   return valor > 10;
+// }
+
+// const numerosFiltrados = numeros.filter(callbackFilter);
+// console.log(numerosFiltrados);
+
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const numerosFiltrados = numeros.filter((valor) => valor > 10);
+//console.log(numerosFiltrados);
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com A
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Leticia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
+//const pessoasComNomeGrande = pessoas.filter((obj) => obj.nome.length >= 7);
+const pessoasComMaisDeCinquentaAnos = pessoas.filter((obj) => obj.idade > 50);
+const nomeTerminaComA = pessoas.filter((obj) => {
+  return obj.nome.toLowerCase().endsWith("a");
+});
+console.log(nomeTerminaComA);
+//console.log(pessoasComMaisDeCinquentaAnos);
