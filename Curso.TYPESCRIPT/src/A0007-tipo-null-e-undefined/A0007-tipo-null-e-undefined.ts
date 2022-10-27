c507793143328b5c5f8765dbcf32b21f29e7d133
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-27 11:12:20 -0300

    Tipos null e undefined

---
+++ Curso.TYPESCRIPT/src/A0007-tipo-null-e-undefined/A0007-tipo-null-e-undefined.ts
@@ -0,0 +1,30 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function creatPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string,
+  lastName?: string,
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squereOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squereOfTwoNumber = squereOf(2);
+const squereOfTwoString = squereOf('2');
+
+if (squereOfTwoNumber === null) {
+  console.log('conta invalida');
+} else {
+  console.log(squereOfTwoNumber * 100);
+}
