c0f8b430361d0b2086be9cfa3e74c8c7b06cdb7c
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-27 15:45:35 -0300

    Tipo Enum

---
+++ Curso.TYPESCRIPT/src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,25 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+  // eslint-disable-next-line prettier/prettier
+}
+
+enum Cores {
+  Roxo = 'Roxo',
+  Verde = 201,
+  Rosa,
+}
+
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(123456789);
+
+
+//console.log(Cores);
+//console.log(Cores.VERMELHO);
+//console.log(Cores[10]);
+//console.log(Cores.Roxo);
