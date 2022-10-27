2d8cb7d65a71579292cba9b702f2a1354204ad4e
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-27 10:26:03 -0300

    Tipo Tuple

--- Curso.TYPESCRIPT/src/A0006-tipo-tuple/A0006-tipo-tuple.ts
+++ Curso.TYPESCRIPT/src/A0006-tipo-tuple/A0006-tipo-tuple.ts
@@ -1,13 +1,21 @@
+// Tuple
+// eslint-disable-next-line prettier/prettier
 const dadosCliente1: readonly [number, string] = [1, 'Gilson'];
 const dadosCliente2: [number, string, string] = [1, 'Gilson', 'Oliveira'];
-// eslint-disable-next-line prettier/prettier
 const dadosCliente3: [number, string, string ?] = [1, 'Gilson'];
 const dadosCliente4: [number, string, ...string []] = [1, 'Gilson'];
 
-dadosCliente1[0] = 100;
-dadosCliente1[1] = 'Alice';
+//dadosCliente1[0] = 100;
+//dadosCliente1[1] = 'Alice';
 
 console.log(dadosCliente1);
 console.log(dadosCliente2);
 console.log(dadosCliente3);
 console.log(dadosCliente4);
+
+//readonly array
+const array1: readonly string[] = ['Gilson'];
+const array2: ReadonlyArray<string> = ['Gilson'];
+
+console.log(array1);
+console.log(array2);
