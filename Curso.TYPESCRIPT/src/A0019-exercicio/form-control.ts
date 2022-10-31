ca014f858d932914ce0408e7ecea02f48d8a589e
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-31 09:29:44 -0300

    Validando Formulári

---
+++ Curso.TYPESCRIPT/src/A0019-exercicio/form-control.ts
@@ -0,0 +1,56 @@
+import isEmail from 'validator/lib/isEmail';
+
+const SHOW_ERROR_MESSAGES = 'show-error-message';
+
+// eslint-disable-next-line prettier/prettier
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
+
+form.addEventListener('submit', function(event: Event) {
+  event.preventDefault();
+  hideErrorMessages(this);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+  checkEqualPasswords(password, password2);
+  if(shouIdSendForm(this)) console.log('Formulário Enviado');
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach(input => {
+    if (!input.value)
+    showErrorMessage(input, 'Campo não pode ficar vazio')
+  });
+}
+
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) showErrorMessage(input, 'Email invalido');
+}
+
+function checkEqualPasswords(password: HTMLInputElement, password2: HTMLInputElement) {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'Senhas não batem');
+    showErrorMessage(password2, 'Senhas não batem');
+  }
+}
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form.querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+  .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+  function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector('.error-message') as HTMLSpanElement;
+   errorMessage.innerText = msg;
+   formFields.classList.add(SHOW_ERROR_MESSAGES);
+  }
+
+  function shouIdSendForm(form: HTMLFormElement): boolean {
+    let send = true;
+    form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach(() => (send = false));
+    return send;
+  }
