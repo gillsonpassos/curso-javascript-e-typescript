be20d1e88141d5a9ebb07549ede10da673cb311e
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-03 14:33:40 -0300

    Keyof e typeof

---
+++ Curso.TYPESCRIPT/src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -0,0 +1,16 @@
+type CoresObj = typeof coresObj;
+type CoresChaves = keyof CoresObj;
+
+const coresObj = {
+  vermelho: 'red',
+  azul: 'blue',
+  verde: 'green',
+  roxo: 'purple',
+};
+
+function traduzirCor(cor: CoresChaves, cores: CoresObj) {
+  return cores[cor];
+}
+console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('verde', coresObj));
+console.log(traduzirCor('roxo', coresObj));
