01130fd98979a01dcabcaea29866bc9ef36a30e4
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-07 09:47:06 -0300

    Utilit Types

---
+++ Curso.TYPESCRIPT/src/A0050-generics-8/A0050-generics-8.ts
@@ -0,0 +1,66 @@
+// Record
+const objeto1: Record<string, string | number> = {
+  nome: 'Patrick',
+  sobrenome: 'Estrela',
+  idade: 30,
+};
+console.log(objeto1);
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
+
+// Required
+type PessoaRequired = Required<PessoaProtocol>;
+
+// Partial
+type PessoaPartial = Partial<PessoaRequired>;
+// Readonly
+type PessoaReadonly = Readonly<PessoaRequired>;
+// Pick
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaRequired = {
+  nome: 'Patrick',
+  sobrenome: 'Estrela',
+  idade: 30,
+};
+
+console.log(objeto2);
+
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
+
+//
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'aasasdad232135d4d4d54635',
+  nome: 'sirigueijo',
+  idade: 100,
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
+
+const accountApi = mapAccount(accountMongo);
+console.log('API:');
+console.log(accountApi);
+
+// Module mode
+export default 1;
